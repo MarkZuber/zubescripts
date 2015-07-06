f24117cdbed5d037da3d6ab8b1ca469849ed5d30
Mark Zuber <mzuber@ntdev.microsoft.com>
2015-07-06 11:12:20 -0700

    Add chocolatey setup and environment init scripts

---
+++ chocsetup.cmd
@@ -0,0 +1,3 @@
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+cinst --confirm %~dp0\chockpackages.config
---
+++ env_init.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+rem Initialize VS Environment Variables
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
+
+set dircmd=/ogn /p
+
+if NOT "%1" == "" (
+  cd /d "%1"
+)
+
