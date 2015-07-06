dcaa5358effaf8c0666d25b36dfa698a4deb8781
Mark Zuber <mzuber@ntdev.microsoft.com>
2015-07-06 11:18:13 -0700

    Add some echo printing...

--- env_init.cmd
+++ env_init.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+echo Initializing environment...
+
 rem Initialize VS Environment Variables
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
 
@@ -9,3 +11,4 @@ if NOT "%1" == "" (
   cd /d "%1"
 )
 
+echo Environment Created.  Welcome :D
