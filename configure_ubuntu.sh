497b4c92ef29ca886271038eabf5b2c096aa9103
Mark Zuber <mark@zube.com>
2018-05-21 20:13:33 -0700

    fix nodejs on ubuntu config

--- configure_ubuntu.sh
+++ configure_ubuntu.sh
@@ -32,15 +32,17 @@ sudo apt-get install -y smbclient
 sudo apt-get install -y libavcodec-extra
 sudo apt-get install -y curl libunwind8 gettext
 
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-artful-prod artful main" > /etc/apt/sources.list.d/dotnetdev.list'
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/prod.list 
+sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1.4
+sudo apt-get install -y dotnet-sdk-2.1.200
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudu apt-get install -y npm
 sudo npm install -g yo
 
 sudo apt-get install -y golang
