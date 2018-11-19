b3203244aac1d57d87741676b32cbb787776523f
Mark Zuber <mark@zube.com>
2018-11-18 20:41:59 -0800

    wsl setup updates

--- wsl_setup.sh
+++ wsl_setup.sh
@@ -3,37 +3,49 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install -y curl libunwind8 gettext
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-get update
-sudo apt-get install dotnet-sdk-2.0.2
+# install winbind and support lib to ping WINS hosts
+sudo apt install -y winbind libnss-winbind
+# need to append to the /etc/nsswitch.conf file to enable if not already done ...
+if ! grep -qc 'wins' /etc/nsswitch.conf ; then
+  sudo sed -i '/hosts:/ s/$/ wins/' /etc/nsswitch.conf
+fi
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+# set the DISPLAY variable to point to the XServer running on our Windows PC
+echo >> ~/.bashrc
+echo "export DISPLAY=:0" >> ~/.bashrc
+
+sudo apt-get install -y curl libunwind8 gettext
+# dotnet core
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-2.1
+
 sudo apt-get install -y nodejs
+sudu apt-get install -y npm
 sudo npm install -g yo
 
 sudo apt-get install -y golang
 sudo apt-get install -y default-jdk
+sudo apt-get install -y screenfetch x11-utils cmake
 
 sudo apt-get -y install ssh openssh-server
 sudo apt-get install -y povray povray-examples
-
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
-sudo apt-get install -y apt-transport-https
-sudo apt-get update && sudo apt-get install -y azure-cli
 
 # from https://github.com/magicmonty/bash-git-prompt
 cd ~
 git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
 
 # add the following to ~/.bashrc
-# LS_COLORS='ow=01;36;40'
-# export LS_COLORS
-# GIT_PROMPT_ONLY_IN_REPO=1
-# source ~/.bash-git-prompt/gitprompt.sh
+echo "LS_COLORS='ow=01;36;40'" >> ~/.bashrc
+echo "export LS_COLORS" >> ~/.bashrc
+echo "GIT_PROMPT_ONLY_IN_REPO=1" >> ~/.bashrc
+echo "source ~/.bash-git-prompt/gitprompt.sh" >> ~/.bashrc
+
+git config --global user.email "mark@zube.com"
+git config --global user.name "Mark Zuber"
+git config --global credential.helper store
 
 
 
